1253a99a3ffdef33a9b1a78a472fa9817d7c231f
Asif Zaman <zero362001@gmail.com>
2024-10-15 19:11:16 +0530

    Update config.h

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "$TERMINAL", NULL };
 
 ResourcePref resources[] = {
 		{ "font",               STRING,  &font },
